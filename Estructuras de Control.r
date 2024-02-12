a28323a22aafd2fe0c847978370f4ff028ab7c32
Agustin Durand <agustindurand.98@gmail.com>
2024-02-12 18:35:46 -0300

    if

---
+++ Estructuras de Control.r
@@ -0,0 +1,29 @@
+
+#ESTRUCTURAS DE CONTROL
+#IF - IF ELSE - IFELSE 
+
+        
+  x <- 10
+  
+
+  if (x == 10) {
+    print("Linea ejecutada")
+  } else {
+    print ("Linea no ejecutada")
+  }
+  
+  
+  Y <- 6
+  
+   if (!is.numeric(x)) { #Por el Operador ! Se vuelve su valor inverso
+     print ("Esto es un numero")
+   } else{
+     print ("No es un Numero")
+   }
+  
+  nombre <- "Tony"
+  ifelse (nombre == "Tony" ,"Hola Tony", "Donde esta Tony?") #Una sola linea de codigo equivalente a la estructura if else
+  
+  
+  
+  
